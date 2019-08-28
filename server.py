5991f962259387519f283e1a6670469f23008289
nakayashiki <tnakayashiki01@gmail.com>
2019-08-28 21:41:05 +0900

    handle some basic requests

--- server.py
+++ server.py
@@ -1,14 +1,55 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
 
 class Hander(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.position = None
+
+        # Time limit
+        self.timelimit = [0 for _ in range(2)]
+        self.byoyomi = 0
+
+        # Clients
+        self.clients = []
+
+        super().__init__(*args, **kwargs)
+
     def do_GET(self):
         # ToDo
         pass
 
     def do_POST(self):
-        # ToDo
-        pass
+        if self.path == '/match':
+            length = self.headers.get('content-length')
 
+            if length is not None:
+                nbytes = int(length)
+                post_data = self.rfile.read(nbytes)
+                post_data = post_data.decode('utf-8')
+                post_data = urllib.parse.parse_qs(post_data)
+
+                if len(self.clients) >= 2:
+                    self.send_response(406)
+                    self.send_header('Content_Type', 'text/plain; charset=utf-8')
+                    self.end_headers()
+                    self.wfile.write('The game is already held.'.encode('utf-8'))
+
+                if not 'name' in post_data:
+                    self.send_response(406)
+                    self.send_header('Content_Type', 'text/plain; charset=utf-8')
+                    self.end_headers()
+                    self.wfile.write('You send a request but name field is None.'.encode('utf-8'))
+
+                if not 'author' in post_data:
+                    self.send_response(406)
+                    self.send_header('Content_Type', 'text/plain; charset=utf-8')
+                    self.end_headers()
+                    self.wfile.write('You send a request but author field is None.'.encode('utf-8'))
+
+                self.send_response(200)
+                self.send_header('Content_Type', 'text/plain; charset=utf-8')
+                self.end_headers()
+                self.wfile.write('OK'.encode('utf-8'))
 
 def main(port=8000):
     server_address = ('localhost', port)
