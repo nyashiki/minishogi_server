9073f20f4725087e079e8340a6e56805cd7b151d
nakayashiki <tnakayashiki01@gmail.com>
2019-08-28 23:03:42 +0900

    global game object

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+from http.cookies import SimpleCookie
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.parse
+import uuid
 
-class Hander(BaseHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
+class Game:
+    def __init__(self):
         self.position = None
 
         # Time limit
@@ -12,13 +14,20 @@ class Hander(BaseHTTPRequestHandler):
         # Clients
         self.clients = []
 
-        super().__init__(*args, **kwargs)
+# Global game object
+game = Game()
 
+class Client:
+    def __init__(self):
+        self.id = None
+
+class Hander(BaseHTTPRequestHandler):
     def do_GET(self):
-        # ToDo
-        pass
+        self.wfile.write(b'Position, kif and something will be here.')
 
     def do_POST(self):
+        global game
+
         if self.path == '/match':
             length = self.headers.get('content-length')
 
@@ -28,28 +37,38 @@ class Hander(BaseHTTPRequestHandler):
                 post_data = post_data.decode('utf-8')
                 post_data = urllib.parse.parse_qs(post_data)
 
-                if len(self.clients) >= 2:
+                if len(game.clients) >= 2:
                     self.send_response(406)
                     self.send_header('Content_Type', 'text/plain; charset=utf-8')
                     self.end_headers()
                     self.wfile.write('The game is already held.'.encode('utf-8'))
+                    return
 
                 if not 'name' in post_data:
                     self.send_response(406)
                     self.send_header('Content_Type', 'text/plain; charset=utf-8')
                     self.end_headers()
                     self.wfile.write('You send a request but name field is None.'.encode('utf-8'))
+                    return
 
                 if not 'author' in post_data:
                     self.send_response(406)
                     self.send_header('Content_Type', 'text/plain; charset=utf-8')
                     self.end_headers()
                     self.wfile.write('You send a request but author field is None.'.encode('utf-8'))
+                    return
 
                 self.send_response(200)
                 self.send_header('Content_Type', 'text/plain; charset=utf-8')
                 self.end_headers()
-                self.wfile.write('OK'.encode('utf-8'))
+
+                client = Client()
+                client.id = str(uuid.uuid4())
+                game.clients.append(client)
+
+                print(game.clients)
+
+                self.wfile.write(client.id.encode('utf-8'))
 
 def main(port=8000):
     server_address = ('localhost', port)
