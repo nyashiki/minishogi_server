4e023a15cff93d32371eff5e0c2c195fd5836334
nyashiki <Nyashiki@users.noreply.github.com>
2019-08-29 18:30:18 +0900

    Basics (#1)
    Implement basic functions.
    
    This program now provides
      - A server that two clients connect to and if there are two clients a match starts
      - A client that connects to the server

---
+++ client.py
@@ -0,0 +1,68 @@
+import simplejson as json
+import socketio
+import subprocess
+
+def main(ip='localhost', port=8000):
+    with open('client.json') as f:
+        config = json.load(f)
+
+    usi_engine = subprocess.Popen(config['command'].split(), cwd=config['cwd'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    usi_engine.stdin.write(b'usi\n')
+    usi_engine.stdin.flush()
+
+    engine_info = { }
+
+    while True:
+        output = usi_engine.stdout.readline()
+        output = output.decode('utf-8').split()
+
+        if output[0] == 'id':
+            engine_info[output[1]] = output[2]
+
+        if output[0] == 'usiok':
+            break
+
+    sio = socketio.Client()
+
+    @sio.event
+    def connect():
+        sio.emit('usi', engine_info)
+
+    @sio.on('error')
+    def error(message):
+        print('ERROR: {}'.format(message))
+        exit(0)
+
+    @sio.on('info')
+    def info(message):
+        print('INFO: {}'.format(message))
+
+    @sio.on('nextmove')
+    def nextmove(data):
+        sfen_position = 'position sfen ' + data['position'] + '\n'
+
+        usi_engine.stdin.write(sfen_position.encode('utf-8'))
+        usi_engine.stdin.flush()
+
+        command = 'go btime {} wtime {} byoyomi {}\n'.format(data['btime'], data['wtime'], data['byoyomi'])
+        usi_engine.stdin.write(command.encode('utf-8'))
+        usi_engine.stdin.flush()
+
+        while True:
+            output = usi_engine.stdout.readline()
+            output = output.decode('utf-8').split()
+
+            if len(output) > 0 and output[0] == 'bestmove':
+                sio.emit('bestmove', output[1])
+
+    @sio.event
+    def disconnect():
+        print('disconnected from server')
+
+    url = 'http://{}:{}'.format(ip, port)
+    sio.connect(url)
+    sio.wait()
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,111 @@
+import eventlet
+import minishogilib
+import simplejson as json
+import socketio
+
+class Game:
+    def __init__(self):
+        self.position = None
+
+        # Time limit
+        self.timelimit = [0 for _ in range(2)]
+        self.byoyomi = 0
+
+        # Clients
+        self.clients = []
+
+class Client:
+    def __init__(self):
+        self.sid = None
+
+def main(port=8000):
+    game = Game()
+
+    sio = socketio.Server()
+
+    with open('server.json') as f:
+        config = json.load(f)
+
+    def ask_nextmove(sid):
+        data = {
+            'position': game.position.sfen(True),
+            'btime': config['btime'],
+            'wtime': config['wtime'],
+            'byoyomi': config['byoyomi']
+        }
+
+        sio.emit('nextmove', data, room=sid)
+
+    @sio.on('usi')
+    def usi(sid, data):
+        if len(game.clients) >= 2:
+            sio.emit('error', 'The game has already started.', room=sid)
+            return
+
+        if not 'name' in data:
+            sio.emit('error', 'You sent a request but name field was None.', room=sid)
+            return
+
+        if not 'author' in data:
+            sio.emit('error', 'You sent a request but author field was None.', room=sid)
+            return
+
+        client = Client()
+        client.sid = sid
+
+        game.clients.append(client)
+
+        sio.emit('info', 'Correctly accepted.', room=sid)
+
+        if len(game.clients) == 2:
+            # Two players sit down, so a game is starting
+            # Initialization
+            game.position = minishogilib.Position()
+            game.position.set_start_position()
+
+            # Ask a first move
+            ask_nextmove(game.clients[0].sid)
+
+    # ToDo: @sio.event disconnect()
+
+    @sio.on('bestmove')
+    def bestmove(sid, data):
+        game.position.print()
+
+        color = game.position.get_side_to_move()
+
+        # An unknown player sent 'bestmove' command, so discard it
+        if game.clients[color].sid != sid:
+            return
+
+        sfen_move = data
+
+        # ToDo: check whether the sent move is legal
+
+        # Apply the sent move
+        move = game.position.sfen_to_move(sfen_move)
+        game.position.do_move(move)
+
+        # Is the game end?
+        is_repetition, is_check_repetition = game.position.is_repetition()
+        if is_check_repetition:
+            # ToDo: check repetition
+            pass
+
+        elif is_repetition:
+            # ToDo: repetition
+            pass
+
+        legal_moves = game.position.generate_moves()
+        if len(legal_moves) == 0:
+            # ToDo: no legal moves. It means the current player hast lost.
+            pass
+
+        # Ask the other player to send a next move
+        ask_nextmove(game.clients[1 - color].sid)
+
+    app = socketio.WSGIApp(sio)
+    eventlet.wsgi.server(eventlet.listen(('localhost', port)), app)
+
+if __name__ == '__main__':
+    main()
